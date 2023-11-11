499735aff931c1c14ef0b139b4d2d75708bdc691
SaiCharanBim <107110377+SaiCharanBim@users.noreply.github.com>
2023-11-11 19:40:10 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main ( String [] args){
+      RWOnly obj = new RWOnly();
+     // obj.name = "Acacio";
+      // member variable is private
+      obj.setName("firstObj");
+      System.out.println(obj.getName());
+
+  }
 }
